3f57d7d5311748aaf85a3906513b2b97f0f42a6d
ilasubhash <subashila01@gmail.com>
2024-08-21 19:08:45 +0530

    lab_003

---
+++ src/ex_10082024/lab_003.py
@@ -0,0 +1,5 @@
+print("Hallow world!")
+# Source code = code written by the Humans, Human readable code
+#Source code = we are following some rules
+print("subhash", 1234,"ila", sep="?", end="_")
+print("Veda")
