f5466fc74c491a10aac3ab1cf8069a9c463ba22f
ilasubhash <subashila01@gmail.com>
2024-08-21 17:11:11 +0530

    example

---
+++ src/ex_10082024/lab_002.py
@@ -0,0 +1,16 @@
+# Python program
+print("Hi Subhash")
+
+# Builtsin.py all the importent already built in python
+# These are like utilities
+# print()
+# self - concept is oops - ignore
+# end= '\n' - in the end what you want to do
+# file= none - file ID
+
+
+# *args - unlimited no of arguments *- string , int, float, boolean..
+# sep - how you want to separate the arguments
+print("subhash","Ila",123,2.34,True, sep="-")
+
+
